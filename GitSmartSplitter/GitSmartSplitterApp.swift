8a8983061394a474a4551fe5727c49380dbc1022
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2025-04-08 16:43:07 +0200

    Initial Commit

---
+++ GitSmartSplitter/GitSmartSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSmartSplitterApp.swift
+//  GitSmartSplitter
+//
+//  Created by Lukas Mauffré on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitSmartSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitSmartSplitterTests/GitSmartSplitterTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitSmartSplitterTests.swift
+//  GitSmartSplitterTests
+//
+//  Created by Lukas Mauffré on 08/04/2025.
+//
+
+import Testing
+@testable import GitSmartSplitter
+
+struct GitSmartSplitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
